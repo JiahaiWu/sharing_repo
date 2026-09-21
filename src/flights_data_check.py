94cd57dc49afad90580b6b28b8b840ddb47df165
jiahaiwu <jiahai.wjh@gmail.com>
2026-09-21 11:54:25 +0800

    update

--- src/flights_data_check.py
+++ src/flights_data_check.py
@@ -8,9 +8,17 @@ rand_seed=1024
 data = pd.read_csv('../data/flights.csv')
 # cols: date,flight_id,route,aircraft_type,scheduled_hour,crew_duty_hours,weather_forecast_18h,
 #       weather_actual,atc_slot_delay_min,delayed
-print(data.isna().sum())
-print(data.duplicated().sum())
-print(data.dtypes)
+# print(data.isna().sum())
+# print(data.duplicated().sum())
+# print(data.dtypes)
 
-sns.catplot(x="delayed", kind="count", data=data)
-plt.show()
+# sns.catplot(x="delayed", kind="count", data=data)
+# plt.show()
+
+data = data.sort_values(by="date", ascending=True)
+train_data = data[data["date"]<'2026-07-03']
+test_data = data[data["date"]>'2026-07-02']
+X_train, X_test = train_data.iloc[:, :-1],test_data.iloc[:, :-1]
+y_train, y_test = train_data.iloc[:, -1],test_data.iloc[:, -1]
+
+test_data.to_csv("../data/test_data.csv")
--- src/flights_delay_classify.py
+++ src/flights_delay_classify.py
@@ -6,7 +6,7 @@ sns.set_style('darkgrid')
 # warnings.filterwarnings('ignore')
 import catboost as cb
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 rand_seed=1024
 target_col="delayed"
@@ -15,13 +15,24 @@ def load_data(data_file='../data/flights.csv', target_col=target_col, rand_seed=
     data = pd.read_csv(data_file)
     # contains cols: date,flight_id,route,aircraft_type,scheduled_hour,crew_duty_hours, <br/>
     #                weather_forecast_18h,weather_actual,atc_slot_delay_min,delayed  <br/>
-    data.drop('flight_id', axis=1, inplace=True)
+    data = data.sort_values(by="date", ascending=True)
+    cnt = len(data)
+    data = data.drop_duplicates(keep=False)
+    print(f"origin cnt={cnt}, after drop_duplicates cnt={len(data)}")
+    train_data = data[data["date"]<'2026-07-03']
+    test_data = data[data["date"]>'2026-07-02']
+    # data["month"]=data["date"].apply(lambda x: x.split("-")[1])
+    data.drop(['date'], axis=1, inplace=True)
+    train_data.drop(['date'], axis=1, inplace=True)
+    test_data.drop(['date'], axis=1, inplace=True)
     cate_cols = [x for x in data.columns if data[x].dtype not in [np.float32, np.float64] and x != target_col]
     for col in cate_cols:
         data[col] = pd.Categorical(data[col])
-    X = data.iloc[:, :-1] 
-    y = data.iloc[:, -1]  
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rand_seed, test_size=0.2)
+    X_train, X_test = train_data.iloc[:, :-1],test_data.iloc[:, :-1]
+    y_train, y_test = train_data.iloc[:, -1],test_data.iloc[:, -1]
+    # X = data.iloc[:, :-1] 
+    # y = data.iloc[:, -1]  
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rand_seed, test_size=0.2)
     # X_test1, X_val, y_test1, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=rand_seed)
     # cate_cols_indexs = np.where(X_train.columns.isin(cate_cols))[0]  
     return X_train, X_test, y_train, y_test, cate_cols
@@ -35,6 +46,8 @@ def model_evaluation(pre, y_test):
     print(precision_score(pre, y_test))
     print('{:-^18}'.format('F1-score'))
     print(f1_score(pre, y_test))
+    print('{:-^18}'.format('Roc_Auc_score'))
+    print(roc_auc_score(pre, y_test))
 
 
 def model_building(X_train, X_test, y_train, y_test, cate_cols):
@@ -63,12 +76,12 @@ def model_building(X_train, X_test, y_train, y_test, cate_cols):
             eval_set=(X_val.values.tolist(), y_val.values.tolist()))
     pre = clf.predict(X_test1.values.tolist())
     model_evaluation(pre, y_test1)
-    return clf, pre
+    return clf, np.column_stack((y_test1, pre))
 
 
-def KFold_EM(X_train, X_test, y_train, y_test, cate_cols, clf):
+def KFold_EM(X_train, X_test, y_train, y_test, cate_cols, clf, n_splits=5, rand_seed=rand_seed):
     cate_cols_indexs = np.where(X_train.columns.isin(cate_cols))[0]  
-    folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=rand_seed)
+    folds = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=rand_seed)
     prob_oof = np.zeros(X_train.shape[0])  
     test_pred_prob = np.zeros(X_test.shape[0])  
     for fold_, (trn_idx, val_idx) in enumerate(folds.split(X_train, y_train)):
@@ -83,12 +96,14 @@ def KFold_EM(X_train, X_test, y_train, y_test, cate_cols, clf):
                 early_stopping_rounds=500,
                 eval_set=(xdata_val, ydata_val))
         prob_oof[val_idx] = clf.predict(xdata_val)
-        test_pred_prob += clf.predict(X_test.values.tolist()) / folds.n_splits
+        test_pred_prob += clf.predict(X_test.values.tolist()) / n_splits
     model_evaluation(test_pred_prob.astype(int), y_test)
 
 if __name__=="__main__":
     X_train, X_test, y_train, y_test, cate_cols = load_data()
     clf, pre = model_building(X_train, X_test, y_train, y_test, cate_cols)
-    KFold_EM(X_train, X_test, y_train, y_test, cate_cols, clf)
+    clf.save_model("fligts_delay_trained.model")
+    np.savetxt("fligts_delay_predicted.txt",pre, fmt="%.2f")
+    # KFold_EM(X_train, X_test, y_train, y_test, cate_cols, clf)
     print("end!")
 
